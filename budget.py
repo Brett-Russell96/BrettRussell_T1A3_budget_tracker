56489eae49c5bc837db93f1e20afc8931f5d0d9d
Brett-Russell96 <brett.russell2016@gmail.com>
2023-12-12 18:53:16 +1100

    Added while loop for main_choice after main_menu input

--- budget.py
+++ budget.py
@@ -16,9 +16,34 @@ def main_menu():
     print("5. Log an expense")
     print("6. New user")
     print("7. Switch user")
+    print("8. Exit application")
     choice = input("Please select a number from the above menu: ")
     return choice
 
+
 main_choice = ""
 
-main_menu()
+while main_choice != "8":
+    main_choice = main_menu()
+    if main_choice == "1":
+        pass
+    elif main_choice == "2":
+        pass
+    elif main_choice == "3":
+        pass
+    elif main_choice == "4":
+        pass
+    elif main_choice == "5":
+        pass
+    elif main_choice == "6":
+        pass
+    elif main_choice == "7":
+        pass
+    elif main_choice == "8":
+        continue
+    else:
+        print("Invalid input, please select a number from 1-8.")
+
+
+
+print("Thankyou for using Budget Tracker!")
