916e7699159eb8d4ed6b6f33436511362841d409
Brett-Russell96 <brett.russell2016@gmail.com>
2023-12-14 18:17:36 +1100

    created functions.py to store feature functions

---
+++ functions.py
@@ -0,0 +1,31 @@
+import json 
+import components
+
+
+def add_income():
+    pass
+
+
+def add_expenses():
+    pass
+
+
+def calculate_average():
+    pass
+
+
+def create_budget():
+    pass
+
+
+def log_expense():
+    pass
+
+
+def new_user():
+    pass
+
+
+def switch_user():
+    pass
+
